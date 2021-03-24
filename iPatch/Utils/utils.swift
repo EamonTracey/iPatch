64eb516d81fb0735d54962f8e6a87a8322cede75
EamonTracey <etracey21@regis.org>
2021-03-24 16:09:56 -0400

    revert to dpkg-deb, implement binary extraction

--- iPatch/Utils/utils.swift
+++ iPatch/Utils/utils.swift
@@ -19,7 +19,7 @@ func shell(launchPath: String, arguments: [String]) {
 
 func extractDylibFromDeb(_ debURL: URL) -> URL {
     let tmpDebURL = tmp.appendingPathComponent("deb")
-    shell(launchPath: "/usr/bin/ar", arguments: ["-x", debURL.path, tmpDebURL.path])
+    shell(launchPath: "/usr/local/bin/dpkg-deb", arguments: ["-x", debURL.path, tmpDebURL.path])
     let dylibDir = tmpDebURL.appendingPathComponent("Library/MobileSubstrate/DynamicLibraries").path
     guard let dylibDirEnum = FileManager.default.enumerator(atPath: dylibDir) else { fatalError() }
     guard let dylibPath = (dylibDirEnum.allObjects.filter {
@@ -30,17 +30,20 @@ func extractDylibFromDeb(_ debURL: URL) -> URL {
 
 func extractAppFromIPA(_ ipaURL: URL) -> URL {
     let tmpIPAURL = tmp.appendingPathComponent("ipa")
-    shell(launchPath: "/usr/bin/unzip", arguments: [ipaURL.path, tmpIPAURL.path])
+    shell(launchPath: "/usr/bin/unzip", arguments: [ipaURL.path, "-d", tmpIPAURL.path])
     let appDir = tmpIPAURL.appendingPathComponent("Payload").path
     guard let appDirEnum = FileManager.default.enumerator(atPath: appDir) else { fatalError() }
     guard let appPath = (appDirEnum.allObjects.filter {
-        ($0 as! String).hasSuffix(".dylib")
+        ($0 as! String).hasSuffix(".app")
     }.first as? String) else { fatalError() }
     return URL(fileURLWithPath: "\(appDir)/\(appPath)")
 }
 
 func extractBinaryFromApp(_ appURL: URL) -> URL {
-    fatalError()
+    let infoURL = appURL.appendingPathComponent("Info.plist")
+    let info = NSDictionary(contentsOf: infoURL)!
+    let executableName = info["CFBundleExecutable"] as! String
+    return appURL.appendingPathComponent(executableName)
 }
 
 func extractBinaryFromIPA(_ ipaURL: URL) -> URL {
