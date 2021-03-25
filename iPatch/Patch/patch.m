7f214f24dc6ceb0b2ee7072fb65433757e64021d
EamonTracey <etracey21@regis.org>
2021-03-24 23:23:28 -0400

    write binary to path

--- iPatch/Patch/patch.m
+++ iPatch/Patch/patch.m
@@ -16,11 +16,15 @@ PatchResult patch_binary_with_dylib(NSString *binaryPath, NSString *dylibPath, B
     uint32_t numHeaders = 0;
     headersFromBinary(headers, binary, &numHeaders);
     
-    // Loop through headers, inserting binary load entry into each
+    // Loop through headers
     for (uint32_t i = 0; i < numHeaders; i++) {
         struct thin_header macho = headers[i];
+        // Insert dylib load entry into binary
         insertLoadEntryIntoBinary(dylibPath, binary, macho, LC_LOAD_DYLIB);
     }
+    
+    // Write binary to original binary path
+    [binary writeToFile:binaryPath atomically:NO];
 
     // Successful patch
     return PatchSuccess;
